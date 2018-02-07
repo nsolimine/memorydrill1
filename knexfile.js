310c58c43b0ffd3f0b2e4d6667f7ea5d043a1ad3
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-02-07 13:43:08 -0700

    heroku push

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+require("dotenv").load();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql:///memory1'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
---
+++ migrations/20180207132332_resolution.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('resolution', table => {
+    table.increments('id').primary();
+    table.date('dueDate');
+    table.text('resolution');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('resolution')
+};
